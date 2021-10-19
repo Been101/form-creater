85b6b2f14f8b32c4f9ec46a59a46bebe2c3ed6f4
been <1043609130@qq.com>
2021-10-19 08:24:23 +0800

    feat: 安装依赖

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import Element from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
 
-Vue.config.productionTip = false
-
+// import custom_form from "@/formCreater/components/custom_form";
+Vue.config.productionTip = false;
+Vue.use(Element, {
+  size: "medium", // set element-ui default size
+  // i18n: (key, value) => i18n.t(key, value)
+});
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
